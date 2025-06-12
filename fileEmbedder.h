47c6671783dec8ef5c7f794c94041b914dca3634
xaizko <tonytran177013@gmail.com>
2025-06-12 17:09:52 -0400

    updated type definitions

--- fileEmbedder.h
+++ fileEmbedder.h
@@ -13,11 +13,13 @@ typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
+typedef unsigned _BitInt(128);
 
-#define $8 (int8 *)
-#define $6 (int16)
-#define $2 (int32)
-#define $4 (int64)
+#define $1 (int8 *)
+#define $2 (int16)
+#define $4 (int32)
+#define $8 (int64)
+#define $16 (int128)
 #define $c (char *)
 #define $i (int)
 
