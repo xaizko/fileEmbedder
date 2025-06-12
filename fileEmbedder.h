313e739201a25ad3db217f52404a5b305b2243c4
xaizko <tonytran177013@gmail.com>
2025-06-12 17:10:43 -0400

    fixed int128 def

--- fileEmbedder.h
+++ fileEmbedder.h
@@ -13,7 +13,7 @@ typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
-typedef unsigned _BitInt(128);
+typedef unsigned _BitInt(128) int128;
 
 #define $1 (int8 *)
 #define $2 (int16)
