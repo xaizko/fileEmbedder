386286c047f4c7350580ba4960ddce35a24829c5
xaizko <tonytran177013@gmail.com>
2025-06-14 22:50:49 -0400

    Removed unnecessary macros

--- fileEmbedder.h
+++ fileEmbedder.h
@@ -8,22 +8,11 @@
 #include <assert.h>
 #include <errno.h>
 #include <birchutils.h>
-#include <string.h>
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
-typedef unsigned _BitInt(128) int128;
-
-#define $1 (int8 *)
-#define $2 (int16)
-#define $4 (int32)
-#define $8 (int64)
-#define $16 (int128)
-#define $c (char *)
-#define $i (int)
-
 
 enum e_language {
     c = 0,
