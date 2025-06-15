8e02b354200ef83ec137fee2a778148a0dc28417
xaizko <tonytran177013@gmail.com>
2025-06-14 22:46:22 -0400

    Finished embedding functionality

--- fileEmbedder.c
+++ fileEmbedder.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
     } else if (argc == 3) {
         // Check for assembly flag
         if (strcmp(argv[1], "-a") == 0) {
-            lang = asm;
+            lang = assembly;
             identifier = argv[2];
         } else {
             usage(argv[0]);
@@ -38,33 +38,95 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+// Prints correct usage if bad one is provided
 void usage(char *arg) {
     fprintf(stderr, "Usage: %s [-h | --help | -a] <identifier>\n", arg);
     exit(-1);
 }
 
+// Prints file header
 void printHeader(char *identifier, language lang) {
     switch (lang) {
-	case asm:
-	    printf("%s:\n", identifier);
-	    break;
-	default:
-	    printf("unsigned char %s[] =\n", identifier);
-	    break; 
+    case assembly:
+        // Print the identifier as a label first
+        printf(".section .data\n");
+        printf("%s:\n\t", identifier);
+        break;
+    case c:
+        // For C, print the array declaration first
+        printf("unsigned char %s[] = {\n\t", identifier);
+        break;
+    default:
+        printf("unsigned char %s[] = {\n\t", identifier);
+        break; 
     }
 }
 
+// Prints the actual file data
 void printBody(language lang) {
-    int32 n;
+    int32 n = 0;  
     signed int ret;
     char buf[2];
-    char *c;
+    char *ch;
+    bool isFirst = true;  // Track if this is the first byte
+    bool hasData = false; // Track if we've read any data
 
-    *buf= *(buf+1) = 0;
+    buf[0] = buf[1] = (char)0;  
 
-    while ( (ret = read(0, char *buf, 1)) == 1 ) {
-	n++;
-	c = convert(*buf, lang);
-	assert(c);
+    while ((ret = read(0, buf, 1)) == 1) {
+        hasData = true;
+        
+        if (!isFirst) {
+            if (n % 16 == 0) {
+                if (lang == c) {
+                    printf(",\n\t"); 
+                } else if (lang == assembly) {
+                    printf("\n\t");
+                    isFirst = true;  // Reset first item flag after newline
+                }
+            } else {
+                printf(", ");
+            }
+        }
+        
+        ch = convert(buf[0], lang);
+        assert(ch);        
+        printf("%s", ch);  
+        
+        isFirst = false;
+        n++;
+        buf[0] = buf[1] = (char)0;
     }
+
+    // Only add closing syntax if we've read some data
+    if (hasData) {
+        switch(lang) {
+        case c:
+            printf("\n};\n");  
+            break;
+        case assembly:
+            printf("\n");
+            break;
+        default:
+            printf("\n");
+        }
+    }
+}
+
+// Converts a character to its hex representation based on language
+char *convert(char ch, language lang) {
+    static char ret[8];  
+    memset(ret, 0, 8); 
+    
+    switch (lang) {
+    case assembly:
+        snprintf(ret, 7, "0x%.02hhx", ch);
+        break;
+    default:
+    case c:
+        snprintf(ret, 7, "0x%.02hhx", ch); 
+        break;
+    }
+
+    return ret;
 }
